29a9398c86391ee86ac9b3acbd2b6563d758804d
Alex Semenchuk <Semenjc@gmail.com>
2024-12-03 13:56:17 +0100

    3files

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-console.log ('Hello, Hexlet!')
-import _ from 'lodash';
-console.log(_.last(['one', 'two']));
+console.log("Hello, Hexlet!");
+import _ from "lodash";
+console.log(_.last(["one", "two"]));
 
+console.log(_.capitalize("hekke"));
 
+a = 2;
+b = 3;
+c = a + b - 250;
+console.log(c);
 
-console.log (_.capitalize ('hekke'));
+(a = 4), (b = 3);
+c = a + b;
+console.log(c);
+
+a = 3;
+b = 4;
+c = a + b;
+console.log(c);
